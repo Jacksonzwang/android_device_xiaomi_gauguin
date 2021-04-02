a67313924e23ff342b7b34042a246e1223472038
Hridya Valsaraju <hridya@google.com>
2021-04-02 11:33:45 -0700

    gauguin: Set PRODUCT_SET_DEBUGFS_RESTRICTIONS
    Starting with Android R launched devices, debugfs cannot be mounted in
    production builds. In order to avoid accidental debugfs dependencies
    from creeping in during development with userdebug/eng builds, the
    build flag PRODUCT_SET_DEBUGFS_RESTRICTIONS can be set by vendors to
    enforce additional debugfs restrictions for userdebug/eng builds. The
    same flag will be used to enable sepolicy neveallow statements to
    prevent new permissions added for debugfs access.
    
    Test: build, boot
    Bug: 184381659
    Change-Id: I45e6f20c886d467a215c9466f3a09965ff897d7e

--- device.mk
+++ device.mk
@@ -120,6 +120,9 @@ PRODUCT_PACKAGES += \
 # Data Services
 $(call inherit-product, vendor/qcom/opensource/dataservices/dataservices_vendor_product.mk)
 
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
