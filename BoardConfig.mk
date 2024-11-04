ab1695eaaa0739011d150b01ba1aaaeac1274016
Jacksonzwang <jacksonzwang@yeah.net>
2024-11-04 19:57:12 +0800

    Revert "gauguin: Use default AOSP clang for compilation"
    This reverts commit c25927f066e7005bbb4e3be38792b563c8eec869.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,9 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/gauguin
-TARGET_KERNEL_CONFIG := gauguin_defconfig
+TARGET_KERNEL_CONFIG := vendor/gauguin_user_defconfig
+TARGET_KERNEL_LLVM_BINUTILS := false
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/kernel/linux-x86/clang-r416183b
 
 # Platform
 TARGET_BOARD_PLATFORM := lito
