ef8426592ef2d5d27fb65e7c022998c922f122c8
Jacksonzwang <jacksonzwang@yeah.net>
2024-11-04 20:46:53 +0800

    gauguin: use clang-r383902 for compile older kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/gauguin
-TARGET_KERNEL_CONFIG := gauguin_defconfig
+TARGET_KERNEL_CONFIG := vendor/gauguin_user_defconfig
+TARGET_KERNEL_LLVM_BINUTILS := false
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/kernel/linux-x86/clang-r383902
 
 # Platform
 TARGET_BOARD_PLATFORM := lito
